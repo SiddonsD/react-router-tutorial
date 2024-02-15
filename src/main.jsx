7c40b476bd2251bdbc101316d1c37e75c9894ed0
SiddonsD <denise.siddons@gmail.com>
2024-02-15 13:53:51 +0800

    update NavBar and link to page components in outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Here we import our pages
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+
+// Here we import our components
+import NavBar from "./components/NavBar.jsx";
+
+// Here we create our router and tell it what pages to render at what path
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+  //   element: <HomePage />,
+  // },
+  // {
+  //   path: "/about",
+  //   element: <AboutPage />,
+  // },
+  // {
+  //   path: "/contact",
+  //   element: <ContactPage />,
+
+    // Putting our NavBar as the main component will cause the children to render in the <Outlet />
+    element: <NavBar />,
+    children: [
+      { path: "/", element: <HomePage />},
+      { path: "/about", element: <AboutPage />},
+      { path: "/contact", element: <ContactPage />},
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* here we wrap our app in the router provider so the pages render */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
